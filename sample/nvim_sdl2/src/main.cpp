07f42876f84d0dbfb61348df37a8690358f2154b
etorth <anhonghe@gmail.com>
2019-11-27 17:51:48 +0000

    not draw the bg picture

--- sample/nvim_sdl2/src/main.cpp
+++ sample/nvim_sdl2/src/main.cpp
@@ -115,21 +115,21 @@ int main()
 
     const size_t window_width  = 800;
     const size_t window_height = 600;
-    const size_t widget_width  = 600;
-    const size_t widget_height = 400;
+    const size_t widget_width  = window_width;
+    const size_t widget_height = window_height;
 
     sdl_device sdl_dev(window_width, window_height, "./font.ttf", 15);
     nvim_sdlwidget widget(&socket, &sdl_dev, widget_width, widget_height);
 
-    auto surf = IMG_Load("./bg.png");
-    if(surf == nullptr){
-        throw std::runtime_error(std::string("IMG_Load() failed: ") + IMG_GetError());
-    }
-
-    auto ptex = SDL_CreateTextureFromSurface(sdl_dev.m_renderer, surf);
-    if(ptex == nullptr){
-        throw std::runtime_error(std::string("SDL_CreateTextureFromSurface() failed: ") + SDL_GetError());
-    }
+    // auto surf = IMG_Load("./bg.png");
+    // if(surf == nullptr){
+    //     throw std::runtime_error(std::string("IMG_Load() failed: ") + IMG_GetError());
+    // }
+    //
+    // auto ptex = SDL_CreateTextureFromSurface(sdl_dev.m_renderer, surf);
+    // if(ptex == nullptr){
+    //     throw std::runtime_error(std::string("SDL_CreateTextureFromSurface() failed: ") + SDL_GetError());
+    // }
 
     while(!process_event(&widget)){
         SDL_SetRenderDrawColor(sdl_dev.m_renderer, 0, 0, 0, 255);
@@ -144,6 +144,6 @@ int main()
         SDL_Delay(10);
     }
 
-    SDL_DestroyTexture(ptex);
+    // SDL_DestroyTexture(ptex);
     return 0;
 }
